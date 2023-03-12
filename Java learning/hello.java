7c3554364fb64cbe9a395b580d0556d0b06efca7
Harsh Raj <116838873+me-harsh@users.noreply.github.com>
2023-03-12 17:44:02 +0530

    added a hel function to owr hello code

--- Java learning/hello.java
+++ Java learning/hello.java
@@ -3,4 +3,10 @@ public class hello {
         System.out.print("just a print out statement ");
         
     }
+    public static void hel(int adding) {
+    
+        System.out.println("i am fine thanx");
+        
+        return ;
+    }
 }
