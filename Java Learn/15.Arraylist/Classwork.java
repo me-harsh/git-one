418d8084a8e5e9e67de3c7598d3ed2202fa4ab9f
Harsh Raj <116838873+me-harsh@users.noreply.github.com>
2024-07-27 16:48:13 +0530

    added remaining files of js and java

---
+++ Java Learn/15.Arraylist/Classwork.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+public class Classwork {
+    public static void main (String args[]){
+        ArrayList<Integer> list = new ArrayList<>();
+        //INFO angular brackets <> are there to represet what type of data you want to store.
+        //INFO vectors in C++ is similar to ArrayList in Java.
+        //INFO ArrayList is part of Java Collection Framework.
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+
+        System.out.println(list);
+        
+
+    }
+}
